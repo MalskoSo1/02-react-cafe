1d7e5eaa0d44ddd702cf4bdce0ac4f14ff767eea
Ліза Гоцко <filani9818@gmail.com>
2025-12-11 12:31:57 +0200

    2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
+// Нормалізація стилів
+import "modern-normalize";
+// Глобальні стилі (додатково)
+// import "./global.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
